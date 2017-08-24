a959d9226cec40eb49a8754d6c98b253ab1f7d46
gianLuke <sanar@live.it>
2017-08-24 17:41:48 +0200

    Message class added

---
+++ src/main/java/Messages.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+/**
+ * Created by gianluke on 24/08/17.
+ *
+ * This is the class containing the different kinds of messages
+ * exchange among nodes
+ */
+public class Messages {
+
+
+}
