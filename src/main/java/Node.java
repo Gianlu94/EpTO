23820ba710da6a7e29a8938824d24508bf418f19
gianLuke <sanar@live.it>
2017-08-24 17:28:37 +0200

    PSS and Node skeleton added

---
+++ src/main/java/Node.java
@@ -0,0 +1,12 @@
+import akka.actor.UntypedActor;
+
+/**
+ * Created by gianluke on 24/08/17.
+ * This is the class that represents a node of the network
+ */
+public class Node extends UntypedActor {
+
+	public void onReceive(Object Message) throws  Exception{
+
+	}
+}
---
+++ src/main/java/PSS.java
@@ -0,0 +1,13 @@
+import akka.actor.UntypedActor;
+
+/**
+ * Created by gianluke on 24/08/17.
+ *
+ */
+
+public class PSS extends UntypedActor {
+
+	public void onReceive (Object Message) throws Exception{
+
+	}
+}
