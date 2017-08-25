b7e2d4a89cddc1517c5f3ef5771aa6780dcc0865
gianLuke <sanar@live.it>
2017-08-25 19:48:54 +0200

    Global clock added methods

--- src/main/java/GlobalClock.java
+++ src/main/java/GlobalClock.java
@@ -1,7 +1,13 @@
-package PACKAGE_NAME;
-
 /**
  * Created by gianluke on 25/08/17.
  */
 public class GlobalClock {
+
+	public static boolean isDeliverable (Event m){
+		return m.getTtl() > Global.TTL;
+	}
+
+	public static long getClock(){
+		return System.currentTimeMillis();
+	}
 }
