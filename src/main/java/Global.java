77cced32211bbc578b4ba03eb0a31812948a4f4a
gianLuke <sanar@live.it>
2017-08-31 11:42:52 +0200

    Path for log

--- src/main/java/Global.java
+++ src/main/java/Global.java
@@ -1,5 +1,7 @@
 import akka.actor.ActorRef;
 
+import java.util.Vector;
+
 /**
  * Created by gianluke on 25/08/17.
  */
@@ -16,4 +18,7 @@ public class Global {
 	public static double D;         //drift
 
 	public static ActorRef pss;     //pss
+
+	public static String pathOutPut = "./output/";
+	public static String pathNode = pathOutPut + "Node-";
 }
